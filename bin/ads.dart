c2b9c0f4f56fed16c9e2f45a9f05a55aec282ec8
Jack DuPlessis <jduplessis294@gmail.com>
2018-01-14 00:03:27 -0500

    Fix ads.dart for heroku

--- bin/ads.dart
+++ bin/ads.dart
@@ -6,6 +6,6 @@ import 'package:ads/src/ad_server.dart';
 
 Future main(List<String> args) async {
   AdServer adServer = new AdServer();
-  var server = await HttpServer.bind(args[0], 80);
+  var server = await HttpServer.bind(args[0], int.parse(args[1]));
   server.listen(await adServer.handle);
 }
